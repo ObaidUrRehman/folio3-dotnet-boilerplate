857746a6f46ab44d97019e8e23aa857778cb9f3b
muneshkumar578 <muneshkumar578@gmail.com>
2021-07-13 21:58:47 +0500

    Sbp.Service: Add ServiceException class

---
+++ src/Folio3.DotNet.Sbp/Folio3.DotNet.Sbp.Service/Exceptions/ServiceException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Folio3.DotNet.Sbp.Service.Exceptions
+{
+    public class ServiceException : Exception
+    {
+        public ServiceException(string message = null, Exception innerException = null)
+            : base(message, innerException) { }
+    }
+}
