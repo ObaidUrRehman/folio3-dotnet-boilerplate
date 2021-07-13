cc237359003fe69a7e2dcf3860351556342f8b61
muneshkumar578 <muneshkumar578@gmail.com>
2021-07-13 17:04:47 +0500

    Sbp.Api: add school connection string

--- src/Folio3.DotNet.Sbp/Folio3.DotNet.Sbp.Api/Startup.cs
+++ src/Folio3.DotNet.Sbp/Folio3.DotNet.Sbp.Api/Startup.cs
@@ -1,7 +1,9 @@
+using Folio3.DotNet.Sbp.Data.School;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,7 @@ namespace Folio3.DotNet.Sbp.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SchoolDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:school"]));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
