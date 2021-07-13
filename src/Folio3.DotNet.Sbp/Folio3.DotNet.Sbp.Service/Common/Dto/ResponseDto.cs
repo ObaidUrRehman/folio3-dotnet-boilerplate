0ae8dfade718e9d8ad3047c23b2d8f02844b98e2
muneshkumar578 <muneshkumar578@gmail.com>
2021-07-13 21:23:51 +0500

    Sbp.Service: Added common ResponseDto. All API endpoints should return this ResponseDto

---
+++ src/Folio3.DotNet.Sbp/Folio3.DotNet.Sbp.Service/Common/Dto/ResponseDto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Folio3.DotNet.Sbp.Service.Common.Dto
+{
+    public class ResponseDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
+        public string TraceId { get; set; }
+
+        public ResponseDto(bool success = false, string message = null)
+        {
+            Message = message;
+            Success = success;
+        }
+
+        public static ResponseDto Successful() => new ResponseDto { Success = true, };
+
+        public static ResponseDto Failure(string message = null, IEnumerable<string> errors = null)
+        {
+            var result = new ResponseDto { Success = false, Message = message };
+            if (errors != null)
+                result.Errors = errors.ToList();
+            return result;
+        }
+
+        public static ResponseDto<T> Failure<T>(string message = null) => new ResponseDto<T> { Success = false, Message = message };
+
+        public static ResponseDto<T> Successful<T>(T result) => new ResponseDto<T> { Result = result, Success = true, };
+
+    }
+
+    public class ResponseDto<T> : ResponseDto
+    {
+        public virtual T Result { get; set; }
+
+        public ResponseDto()
+        { }
+
+        public ResponseDto(T result, bool success = false, string message = null)
+            : base(success, message)
+        {
+            Result = result;
+        }
+    }
+}
