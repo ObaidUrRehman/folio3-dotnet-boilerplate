239956880a9d35134fae5a6dbd492c99839192f3
muneshkumar578 <muneshkumar578@gmail.com>
2021-07-13 16:59:49 +0500

    Add BaseDbContext

---
+++ src/Folio3.DotNet.Sbp/Folio3.DotNet.Sbp.Data/Base/BaseDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Folio3.DotNet.Sbp.Data.Base
+{
+	public class BaseDbContext : DbContext
+	{
+		public BaseDbContext(DbContextOptions options) : base(options)
+		{
+		}
+	}
+}
---
+++ src/Folio3.DotNet.Sbp/Folio3.DotNet.Sbp.Data/Base/Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Folio3.DotNet.Sbp.Data.Base.Entities
+{
+	public abstract class BaseEntity
+	{
+        [Display(Name = "Revisions", AutoGenerateField = false), ScaffoldColumn(false)]
+        public int Version { get; set; } = 1;
+
+        [Display(Name = "Created", AutoGenerateField = false), ScaffoldColumn(false)]
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        [Display(Name = "Updated", AutoGenerateField = false), ScaffoldColumn(false)]
+        public DateTime Updated { get; set; } = DateTime.UtcNow;
+
+        public void Touch()
+        {
+            Version++;
+            Updated = DateTime.UtcNow;
+        }
+    }
+}
