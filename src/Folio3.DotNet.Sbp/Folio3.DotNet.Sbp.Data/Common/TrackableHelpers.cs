cd40daa7ddd41d610f66883edce1e4f5b16e1f89
muneshkumar578 <muneshkumar578@gmail.com>
2021-07-14 15:31:38 +0500

    Sbp.Data: override SaveChanges methods to populate trackable fields.

---
+++ src/Folio3.DotNet.Sbp/Folio3.DotNet.Sbp.Data/Common/TrackableHelpers.cs
@@ -0,0 +1,40 @@
+﻿using Folio3.DotNet.Sbp.Data.Base.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Folio3.DotNet.Sbp.Data.Common
+{
+    public static class TrackableHelpers
+    {
+        /// <summary>
+        /// Walk the changes in a ChangeTracker over changes to TrackableEntity records
+        /// and apply data to the fields
+        /// </summary>
+        /// <param name="changes">changes about to be saved</param>
+        public static void PopulateTrackableFields(ChangeTracker changes)
+        {
+            foreach (var e in changes.Entries<TrackableEntity>())
+            {
+                TrackableEntity te = e.Entity;
+                switch (e.State)
+                {
+                    case EntityState.Added:
+                        break;
+
+                    case EntityState.Modified:
+                        te.Touch();
+                        break;
+
+                    default:
+                    case EntityState.Deleted:
+                    case EntityState.Detached:
+                    case EntityState.Unchanged:
+                        break;
+                }
+            }
+        }
+    }
+}
