baf7334c796888122d804d85767f68cf8459c012
muneshkumar578 <muneshkumar578@gmail.com>
2021-07-20 11:48:37 +0500

    Sbp.UnitTest: Verify audit log changes

--- src/Folio3.DotNet.Sbp/Folio3.Sbp.UnitTest/DataTests/SchoolDbContextTests.cs
+++ src/Folio3.DotNet.Sbp/Folio3.Sbp.UnitTest/DataTests/SchoolDbContextTests.cs
@@ -1,6 +1,8 @@
 ﻿using Folio3.Sbp.Data.AuditLogging;
 using Folio3.Sbp.Data.School;
+using Folio3.Sbp.Data.School.Entities;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -29,6 +31,31 @@ namespace Folio3.Sbp.UnitTest.DataTests
             var student = await SchoolDbContext.Students.AsQueryable().FirstOrDefaultAsync(s => s.ID == StudentId);
 
             Assert.Equal("Carson", student?.FirstMidName);
+        }
+
+        [Fact]
+        public async Task SaveChangesAsync_AddAndDeleteStudent_HasAuditLogs()
+        {
+            Assert.Equal(8, SchoolDbContext.Students.Count());
+
+            var student = new Student
+            {
+                FirstMidName = "Unit Test",
+                LastName = "User",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            SchoolDbContext.Students.Add(student);
+            await SchoolDbContext.SaveChangesAsync();
+
+            Assert.Equal(9, SchoolDbContext.Students.Count());
+            Assert.Equal(1, AuditLogDbContext.AuditLogs.Count());
+            Assert.True(AuditLogDbContext.AuditLogChanges.Count() > 0);
+
+            SchoolDbContext.Students.Remove(student);
+            await SchoolDbContext.SaveChangesAsync();
+
+            Assert.Equal(8, SchoolDbContext.Students.Count());
         }
     }
 }
